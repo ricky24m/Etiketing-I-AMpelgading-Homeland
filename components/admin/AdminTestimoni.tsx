827f4c5220fd4c5bc90aaa77d81e013edf4d4d4c
ricky24m <168514470+ricky24m@users.noreply.github.com>
2025-08-26 06:58:27 +0700

    harusnya bisa

--- components/admin/AdminTestimoni.tsx
+++ components/admin/AdminTestimoni.tsx
@@ -118,7 +118,7 @@ export default function AdminTestimoni() {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
                         d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3m-9 0h10" />
                     </svg>
-                    Hapus
+                    Hapuss
                   </button>
                 </td>
               </tr>
